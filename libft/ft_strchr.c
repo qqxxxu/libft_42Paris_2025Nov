94a6836aab7380fa047a549d799e939bedcd5a54
Qing XU <78740466+qqxxxu@users.noreply.github.com>
2025-12-22 17:47:17 +0100

    Update ft_strchr.c

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/* Note how the if condition is differently carried out than ft_strrchr(). */
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
