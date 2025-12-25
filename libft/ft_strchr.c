ff02bb36b9bfcec12ad122f23f646e88ff4acdb4
Qing XU <78740466+qqxxxu@users.noreply.github.com>
2025-12-25 14:21:01 +0100

    Update ft_strchr.c

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -11,7 +11,11 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-/* This function should find the first occurrence of "c"--different from ft_strrchr(), which is supposed to find the last. */
+/* 1. This function should find the first occurrence of "c"--different from ft_strrchr(), which is supposed to find the last.
+2. One concept that might pose difficulty for understanding is that this function takes an INT instead of an UNSIGNED CHAR
+The reason seems to be an accident in history: https://stackoverflow.com/questions/2394011/why-does-strchr-take-an-int-for-the-char-to-be-found
+But in another function(getc()), an int is returned so that it could represent EOF(-1).
+3. unsigned char: 0 to 255; signed char: -128 to 127.*/
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
